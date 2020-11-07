9d161fe1ab99b39ec5787706744a2c19c1e15559
Molkamimi <benothmen.molka@gmail.com>
2020-11-07 07:24:37 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import AddItems from "./components/Additems";
+import ListItems from "./components/ListItems";
+
+function App() {
+  return (
+    <div className="App">
+      <AddItems />
+      <ListItems />
+    </div>
+  );
+}
+
+export default App;
